9abaae1f0e1ba5de2e7a7ac276f0b5058d68321b
mauriciobordon <mauricio.lb1@puccampinas.edu.br>
2023-11-20 10:54:51 -0300

    adicionada validação na entrada no form e agenda na página de solicitação

--- resources/views/anivers/solicitacao/novafesta.blade.php
+++ resources/views/anivers/solicitacao/novafesta.blade.php
@@ -134,6 +134,37 @@
 
     </div>
 
+    <div>
+        <h1>Agenda</h1>
+
+        <table cellspacing="8" cellpadding="8">
+            <caption class="table-caption">Disponibilidade de Serviços</caption>
+            <thead>
+                <th>Data</th>
+                <th>Dia da semana</th>
+                @for($hora=0; $hora<24; $hora++)
+                    @php
+                        $horario=str_pad($hora, 2, '0', STR_PAD_LEFT)
+                    @endphp
+                    <th>{{ $horario }}h</th>
+                @endfor
+            </thead>
+            <tbody id="agendaBody">
+                @foreach($agenda->take(7) as $dia)
+                    <tr>
+
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+
+        <p> <?php $indicesemana ?> </p>
+
+            <button onclick="mostrarAnteriores()">Semana Anterior</button>
+            <button onclick="mostrarProximos()">Próxima Semana</button>
+
+    </div>
+
 
     <script>
         function validarSelecao() {
@@ -243,6 +274,66 @@
 
         })
 
+
+
+
+
+
+    let elementosExibidos = 0;
+    const agendaCompleta = {!! json_encode($agenda) !!};
+    const totalElementos = {!! json_encode(count($agenda)) !!};
+
+    // Encontrar o índice do elemento cujo "dia" corresponde ao dia atual
+    const diaAtual = '{{ now()->toDateString() }}';
+    const indiceDiaAtual = agendaCompleta.findIndex(dia => dia.dia === diaAtual);
+
+    // Definir elementosExibidos com base no índice do dia atual
+    elementosExibidos = ((indiceDiaAtual >= 0) ? indiceDiaAtual : 0) - (new Date(diaAtual).getDay() == 6 ? 0 : new Date(diaAtual).getDay() + 1);
+    limiteInferior = elementosExibidos;
+
+    function mostrarProximos() {
+        if (elementosExibidos + 6 <= totalElementos)
+        {
+            const proximosElementos = agendaCompleta.slice(elementosExibidos, elementosExibidos + 7);
+
+            const tbody = document.getElementById('agendaBody');
+            tbody.innerHTML = '';
+
+            proximosElementos.forEach(dia => {
+                const row = document.createElement('tr');
+            row.innerHTML += `<td>${dia.dia}</td>`;
+            row.innerHTML += `<td>${dia.diadasemana}</td>`;
+
+            for (let hora = 0; hora < 24; hora++) {
+                const horario = `h${hora.toString().padStart(2, '0')}`;
+                // row.innerHTML += `<td>${dia[horario]}</td>`;
+                if(dia[horario] == 0)
+                    row.innerHTML += `<td>&#x2714</td>`;
+                else
+                    row.innerHTML += `<td>&#x274C</td>`;
+
+            }
+            tbody.appendChild(row);
+        });
+        elementosExibidos += 7;
+        if (elementosExibidos >= totalElementos) {
+            elementosExibidos = totalElementos - (totalElementos % 7 || 7);
+        }
+    }
+}
+
+function mostrarAnteriores() {
+    elementosExibidos -= 14;
+
+    if (elementosExibidos < limiteInferior) {
+        elementosExibidos = limiteInferior;
+    }
+
+    mostrarProximos();
+}
+
+mostrarProximos();
+
     </script>
 
 @endsection
@@ -255,6 +346,12 @@
             margin-top: 20px;
             background-color:rgba(42, 120, 165, 0.692);
         }
+
+        .table-caption {
+            caption-side: top;  /* Posiciona o caption no topo da tabela */
+            margin-bottom: 10px;  /* ou ajuste conforme necessário */
+        }
+
 
     </style>
 @endpush
